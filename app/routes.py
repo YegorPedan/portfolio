2fa5da3bd9c2498f70f586602688509c29b773ea
yegor <wzxv_@outlook.com>
2022-11-13 08:43:22 +0300

    Create: about page with css

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,3 @@ from flask import render_template
 @app.route("/home")
 def index():
     return render_template("index.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
