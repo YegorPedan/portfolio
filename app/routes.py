835daec38bff8043c4dca47ad6fce78a94482ce2
yegor <wzxv_@outlook.com>
2022-11-13 07:39:05 +0300

    Create: route for about page

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,8 @@ from flask import render_template
 @app.route("/home")
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
